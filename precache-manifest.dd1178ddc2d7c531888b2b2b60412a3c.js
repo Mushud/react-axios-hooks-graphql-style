550105d4459fcd8dfdad9324cc58e4d95fd3b134
Mushud <mushudkofiabuu@gmail.com>
2021-05-31 02:01:42 +0000

    Updates

---
+++ precache-manifest.dd1178ddc2d7c531888b2b2b60412a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c911daf109ebcd0c0b085e0ad0e006",
+    "url": "./index.html"
+  },
+  {
+    "revision": "91896fd346279ecda14b",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "4fa4da52c5040c440bdd",
+    "url": "./static/js/2.da8dc5f7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.da8dc5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91896fd346279ecda14b",
+    "url": "./static/js/main.563759b5.chunk.js"
+  },
+  {
+    "revision": "bb1ba57a732655234b30",
+    "url": "./static/js/runtime-main.f6f15464.js"
+  }
+]);
